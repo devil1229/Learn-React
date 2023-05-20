19d499cc7ca68ef1ff2f3dffa03a6cae3c86aabe
Sujit Sable <sujitsable92@gmail.com>
2023-05-20 16:56:27 +0530

    Completed First react Project

---
+++ project1/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Main from "./components/Main"
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+      <div className='container'>
+        <Navbar />
+        <Main /> 
+      </div>
+  )
+}
+
+export default App
---
+++ project1/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar(){
+    return (
+        <nav className='nav-bar'>
+            <img  className='logo' src="/react-icon-small.png" alt="React Logo" />
+           <h2>ReactFacts</h2>
+           <h3>React Course - Project 1</h3>
+       </nav>
+    )
+}
